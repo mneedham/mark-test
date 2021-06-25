039cf0f3aa4ee0f29395e7d4eef14bb620c010e3
Mark Needham <m.h.needham@gmail.com>
2021-06-25 14:40:56 +0100

    Update foo.py

--- foo.py
+++ foo.py
@@ -3,5 +3,5 @@ import os
 
 class Bitemporal(unittest.TestCase):
   def test_foo(self):
-    env_var = os.environ.get("secrets.MARK_KEY")
+    env_var = os.environ.get("MARK_KEY")
     self.assertIsNotNone(env_var, "no env var")
